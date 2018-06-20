7ce80f3919a80bb15cea108de82d346985bc37d1
Andrew Nikonchuk <anikonch@gmail.com>
2018-06-20 18:53:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,5 +21,13 @@ class Song
     song.name = name 
     return song 
   end
-
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    song.save 
+    return song 
+  end
+  
+  
 end
